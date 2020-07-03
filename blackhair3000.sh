22c72f795ebd8772c39b65adbdd2213a762bb639
shw <shw@wooduan.com>
2020-07-03 18:05:36 +0800

    修改nginx.conf 和blackhair3000.sh

--- blackhair3000.sh
+++ blackhair3000.sh
@@ -3,6 +3,7 @@ WORKING_DIR=/root/myenv/virtualenvs/blackhair3000
 ACTIVATE_PATH=/root/.virtualenvs/blackhair3000/bin/activate
 cd ${WORKING_DIR}
 source ${ACTIVATE_PATH}
+gunicorn --workers 3 --bind unix:/tmp/blackhair3000.com.socket blackhair3000.wsgi:application
 exec $@
 
 
